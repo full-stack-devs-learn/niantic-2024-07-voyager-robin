961e3ac0d3ba8fb2d886c9eeacc0f78713ba6d5e
Robin Weaver <strwtch13@gmail.com>
2024-08-14 17:36:28 -0700

    Turning in thymeleaf/bootstrap exercise

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,90 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
     CategoryDao categoryDao = new CategoryDao();
+
+    // http://localhost:8080/categories
+    @GetMapping("/categories")
+    public String getAllCategories(Model model)
+    {
+        ArrayList<Category> categories;
+        categories = categoryDao.getCategories();
+        model.addAttribute("categories", categories);
+
+        return "categories/index";
+    }
+
+    @GetMapping("/categories/{id}")
+    public String getCategoriesById(Model model, @PathVariable int id)
+    {
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+
+        return "categories/details";
+    }
+
+    @GetMapping("/categories/add")
+    public String addCategory(Model model)
+    {
+        model.addAttribute("category", new Category());
+        return "categories/add_edit";
+    }
+
+    @PostMapping("/categories/add")
+    public String addCategory(Model model, @ModelAttribute("category") Category category)
+    {
+        categoryDao.addCategory(category);
+        model.addAttribute("category", category);
+
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/{id}/edit")
+    public String editCategory(Model model, @PathVariable int id)
+    {
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+
+        return "categories/add_edit";
+    }
+
+    @PostMapping("/categories/{id}/edit")
+    public String editCategory(@ModelAttribute("category") Category category,
+                               @PathVariable int id)
+    {
+        category.setCategoryId(id);
+        categoryDao.updateCategory(category);
+
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/{id}/delete")
+    public String deleteCategory(Model model, @PathVariable int id)
+    {
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute(category);
+
+        return "/categories/delete";
+    }
+
+    @PostMapping("/categories/{id}/delete")
+    public String deleteCategory(@PathVariable int id)
+    {
+        categoryDao.deleteCategory(id);
+
+        return "redirect:/categories";
+    }
 }
