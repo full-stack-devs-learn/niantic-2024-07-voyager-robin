6120bb56fd5a43db834cdeaee19f1c9b72772f73
Robin Weaver <strwtch13@gmail.com>
2024-09-03 17:20:54 -0700

    Turning in DOM exercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,17 +1,55 @@
 // add pageTitle
-
+function displayPageTitle()
+{
+    newList = new ShoppingService;
+    pageTitle = newList.getListName();
+    document.title = pageTitle;
+}
 // add groceries
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    document.getElementById('title').innerHTML = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries()
+{
+    // get the list of all groceries
+    const groceries = newList.getShoppingList();
+    // get the parent container
+    const groceryListContainer = document.getElementById("shopping-list");
+    // create new div for grocery item
+    groceries.forEach(item => {
+        createGroceryItem(item, groceryListContainer);
+    });
+}
+
+function createGroceryItem(child, parent)
+{
+    // Create the new divs and assign the correct classes:
+    const listItemDiv = document.createElement("div");
+    const itemDiv     = document.createElement("div");
+    const quantityDiv = document.createElement("div");
+    
+    listItemDiv.classList.add("list-item")
+    itemDiv.classList.add("shopping-list");
+    quantityDiv.classList.add("quantity-container");
+
+
+    itemDiv.textContent     = child.title;
+    quantityDiv.textContent = child.quantity;
+
+    parent.appendChild(listItemDiv)
+
+    listItemDiv.appendChild(itemDiv);
+    listItemDiv.appendChild(quantityDiv);
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
@@ -20,7 +58,7 @@ function displayGroceries() {}
 function markCompleted() {
 }
 
-
+displayPageTitle();
 displayListTitle();
 displayGroceries();
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,6 +1,8 @@
 class ShoppingService {
 
-    getListName() {
+    getListName() 
+    {
+        return "Shopping List";
     }
 
     getShoppingList() {
@@ -16,7 +18,55 @@ class ShoppingService {
                 title: 'Frozen Pizza',
                 quantity: 4,
                 isComplete: true
-            }
+            },
+            {
+                id: 3,
+                title: 'Bagels',
+                quantity: 6,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Sourdough',
+                quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 5,
+                title: 'Chickpeas',
+                quantity: 5,
+                isComplete: true
+            },
+            {
+                id: 6,
+                title: 'Thyme',
+                quantity: 15,
+                isComplete: true
+            },
+            {
+                id: 7,
+                title: 'Pasta',
+                quantity: 5,
+                isComplete: true
+            },
+            {
+                id: 8,
+                title: 'Tomatoes',
+                quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 9,
+                title: 'Cherries',
+                quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 10,
+                title: 'Orange Juice',
+                quantity: 4,
+                isComplete: true
+            },
         ];
     }
 }
