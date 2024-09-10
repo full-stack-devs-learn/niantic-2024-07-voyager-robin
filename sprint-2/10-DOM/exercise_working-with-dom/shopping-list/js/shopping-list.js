aad50c87cf61d91e0eed43a9961f3ba9f1839712
Robin Weaver <strwtch13@gmail.com>
2024-09-10 16:25:37 -0700

    Updating DOM exercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,13 +1,11 @@
-// add pageTitle
 function displayPageTitle()
 {
     newList = new ShoppingService;
     pageTitle = newList.getListName();
     document.title = pageTitle;
 }
-// add groceries
 
-/**
+/*
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
@@ -15,47 +13,59 @@ function displayListTitle() {
     document.getElementById('title').innerHTML = pageTitle;
 }
 
-/**
+/*
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
 function displayGroceries()
 {
-    // get the list of all groceries
     const groceries = newList.getShoppingList();
-    // get the parent container
     const groceryListContainer = document.getElementById("shopping-list");
-    // create new div for grocery item
-    groceries.forEach(item => {
+    groceries.forEach(item => 
+    {
         createGroceryItem(item, groceryListContainer);
     });
 }
 
 function createGroceryItem(child, parent)
 {
-    // Create the new divs and assign the correct classes:
-    const listItemDiv = document.createElement("div");
-    const itemDiv     = document.createElement("div");
-    const quantityDiv = document.createElement("div");
+    const listItemDiv  = document.createElement("div");
+    const itemDiv      = document.createElement("div");
+    const quantityDiv  = document.createElement("div");
+    const quantitySpan = document.createElement("span");
     
-    listItemDiv.classList.add("list-item")
+    listItemDiv.classList.add("list-item");
     itemDiv.classList.add("shopping-list");
     quantityDiv.classList.add("quantity-container");
+    quantitySpan.classList.add("super");
 
+    if (child.isComplete)
+    {
+        listItemDiv.classList.add("complete")
+    }
 
-    itemDiv.textContent     = child.title;
-    quantityDiv.textContent = child.quantity;
-
+    itemDiv.textContent      = child.title;
+    quantitySpan.innerText = 'quantity';
+    quantityDiv.textContent  = child.quantity;
+   
     parent.appendChild(listItemDiv)
 
     listItemDiv.appendChild(itemDiv);
     listItemDiv.appendChild(quantityDiv);
+    quantityDiv.appendChild(quantitySpan);
 }
 
-/**
+/*
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {
+function markCompleted() 
+{
+    const listItems = document.querySelectorAll(".list-item");
+
+    listItems.forEach(listItem => 
+    {
+        listItem.classList.add("complete");
+    })
 }
 
 displayPageTitle();
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -11,61 +11,61 @@ class ShoppingService {
                 id: 1,
                 title: '5 lb sack of potatoes',
                 quantity: 1,
-                isComplete: false
+                isComplete: true
             },
             {
                 id: 2,
                 title: 'Frozen Pizza',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 3,
                 title: 'Bagels',
                 quantity: 6,
-                isComplete: false
+                isComplete: true
             },
             {
                 id: 4,
                 title: 'Sourdough',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 5,
                 title: 'Chickpeas',
                 quantity: 5,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 6,
                 title: 'Thyme',
                 quantity: 15,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 7,
                 title: 'Pasta',
                 quantity: 5,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 8,
                 title: 'Tomatoes',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 9,
                 title: 'Cherries',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
             },
             {
                 id: 10,
                 title: 'Orange Juice',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
             },
         ];
     }
