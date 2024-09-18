69ed067eb9037324b48b8971c2e66e4314c1dcdb
Robin Weaver <strwtch13@gmail.com>
2024-09-17 18:39:18 -0700

    Turning in file io & exceptions exercise

--- sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/application/GradingApplication.java
+++ sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/application/GradingApplication.java
@@ -5,6 +5,14 @@ import com.niantic.services.GradesFileService;
 import com.niantic.services.GradesService;
 import com.niantic.ui.UserInput;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.round;
+
 public class GradingApplication implements Runnable
 {
     private GradesService gradesService = new GradesFileService();
@@ -42,19 +50,130 @@ public class GradingApplication implements Runnable
 
     private void displayAllFiles()
     {
-        // todo: 1 - get and display all student file names
+        File filesDir = new File("files");
+
+        if(filesDir.isDirectory())
+        {
+            List<String> fileNames = Arrays.stream(filesDir.list()).sorted().toList();
+
+            System.out.println("-".repeat(30));
+            System.out.println("List of Files:");
+            System.out.println("-".repeat(14));
+            for (String fileName : fileNames)
+            {
+                System.out.println(fileName);
+            }
+            System.out.println("-".repeat(30));
+        }
+        else
+        {
+            System.out.println("No files/ directory found!");
+        }
+
     }
 
     private void displayFileScores()
     {
-        // todo: 2 - allow the user to select a file name
-        // load all student assignment scores from the file - display all files
+        String choice;
+        File file;
+        List<Assignment> assignments = new ArrayList<Assignment>();
+
+        choice = "files/" + UserInput.FileSelection();
+        file   = new File(choice);
+
+
+        // Read from the chosen file:
+        try(Scanner reader = new Scanner(file))
+        {
+            String studentName = parseStudentName(file.getName());
+
+            // Skip the header line:
+            reader.nextLine();
+            // Run through each line after the header:
+            while(reader.hasNextLine())
+            {
+                // Convert the contents of each line to an assignment object:
+                var line = reader.nextLine();
+                var columns = line.split(",");
+
+                int assignmentIndex    = Integer.parseInt(columns[0]);
+                String firstName       = columns[1];
+                String lastName        = columns[2];
+                String assignmentName  = columns[3];
+                int assignmentScore    = Integer.parseInt(columns[4]);
+
+                var assignmentObject = new Assignment(assignmentIndex, firstName,
+                                        lastName, assignmentName, assignmentScore);
+                // Add the assignment to the list
+                assignments.add(assignmentObject);
+            }
+
+            // Print the output:
+            System.out.println("-".repeat(30));
+            System.out.println(studentName);
+            System.out.println("-".repeat(studentName.length()));
+            System.out.println("All Assignments:\n");
+            for (Assignment assignment : assignments)
+            {
+                System.out.println(assignment);
+            }
+            System.out.println("-".repeat(30));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 
     private void displayStudentAverages()
     {
-        // todo: 3 - allow the user to select a file name
-        // load all student assignment scores from the file - display student statistics (low score, high score, average score)
+        String choice;
+        File file;
+        int lowScore     = 0;
+        int highScore    = 0;
+        double avgScore  = 0;
+        List<Integer> assignmentScore = new ArrayList<>();
+
+        choice = "files/" + UserInput.FileSelection();
+        file   = new File(choice);
+
+
+        // Read from the chosen file:
+        try(Scanner reader = new Scanner(file))
+        {
+            String studentName = parseStudentName(file.getName());
+
+            // Skip the header line:
+            reader.nextLine();
+
+            while(reader.hasNextLine())
+            {
+                var line = reader.nextLine();
+                var columns = line.split(",");
+                assignmentScore.add(Integer.parseInt(columns[4]));
+            }
+
+            lowScore  = assignmentScore.stream().min(Integer::compareTo).get();
+            highScore = assignmentScore.stream().max(Integer::compareTo).get();
+            avgScore  = assignmentScore.stream().mapToDouble(Integer::doubleValue)
+                                       .average().getAsDouble();
+
+            // Print the result:
+            System.out.println("-".repeat(30));
+            System.out.println(studentName);
+            System.out.println("-".repeat(studentName.length()));
+            System.out.println("Assignment Details:\n");
+            System.out.println("High Score:    " + highScore);
+            System.out.println("Low Score:     " + lowScore);
+            System.out.println("Average Score: " + round(avgScore));
+            System.out.println("-".repeat(30));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -75,6 +194,6 @@ public class GradingApplication implements Runnable
     {
         return fileName.replace(".csv", "")
                         .replace("_", " ")
-                        .substring(10);
+                        .substring(10).toUpperCase();
     }
 }
--- sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/ui/UserInput.java
+++ sprint-3/04-file-io/exercises_file-io-exceptions/src/main/java/com/niantic/ui/UserInput.java
@@ -35,4 +35,36 @@ public class UserInput
         System.out.println();
         System.out.println(message);
     }
+
+    public static String FileSelection()
+    {
+        String input;
+
+        System.out.println();
+        System.out.println("-".repeat(30));
+        System.out.println("Enter the Student Number for the report you would like to view:\n");
+        System.out.println(" 1) Anna Williams");
+        System.out.println(" 2) David Jones");
+        System.out.println(" 3) Laura Brown");
+        System.out.println(" 4) Eric Walker");
+        System.out.println(" 5) Joshua Martin");
+
+        input = in.nextLine();
+
+        switch(input)
+        {
+            case "1":
+                return "student_1_anna_williams.csv";
+            case "2":
+                return "student_2_david_jones.csv";
+            case "3":
+                return "student_3_laura_brown.csv";
+            case "4":
+                return "student_4_eric_walker.csv";
+            case "5":
+                return "student_5_joshua_martin.csv";
+            default:
+                return " ";
+        }
+    }
 }
